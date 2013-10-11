75ef82320e45c93a4acbdbdf1426bb1f718f514f
Bradley Jones <jones.bradley@me.com>
2013-10-11 18:21:13 +0100

    Added code to simulate getting an initial number of connected agents

--- Queen/controllers/agents.js
+++ Queen/controllers/agents.js
@@ -4,6 +4,10 @@ exports.list = function(req, res){
 
 // Web Sockets
 io.sockets.on('connection', function (socket) {
+  // Get the current number of connect agents
+  socket.emit('init', { total: 200 });
+
+  // New agent added
   socket.on('newAgent', function (data) {
     socket.broadcast.emit('agent', data);
   });
