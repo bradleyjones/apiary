b534ff7b3a8e0235f731f7621d9cbe547b784092
Bradley Jones <jones.bradley@me.com>
2013-10-13 02:19:00 +0100

    Added persistent storing of current connected agents and display them in a table can also take 'offline' any of the dummy agents

--- Queen/controllers/agents.js
+++ Queen/controllers/agents.js
@@ -2,18 +2,24 @@ exports.list = function(req, res){
   res.render('agents.jade')
 }
 
+// List of all agents
+var allAgents = {};
+
 // Web Sockets
 io.sockets.on('connection', function (socket) {
   // Get the current number of connect agents
-  socket.emit('init', { total: 200 });
+  socket.emit('init', allAgents);
 
   // New agent added
   socket.on('newAgent', function (data) {
+    allAgents[data.id] = data;
     socket.broadcast.emit('agent', data);
   });
 
   // Agent gone offline
-  socket.on('agentOffline', function (data) {
-    socket.broadcast.emit('offline', data)
+  socket.on('agentOffline', function (id) {
+    delete allAgents[id];
+    console.log(allAgents);
+    socket.broadcast.emit('offline', id)
   });
 });
