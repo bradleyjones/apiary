e892979751f454e60dfbed4175b051bd547cd05a
Sam Betts <sam@code-smash.net>
2013-10-16 17:39:27 +0100

    Updated setup tools and manifest to include everything

--- Hive/setup.py
+++ Hive/setup.py
@@ -1,27 +1,27 @@
 import ez_setup
 ez_setup.use_setuptools()
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='Apiary Hive',
-    version='0.1.0',
+    name='ApiaryHive',
+    version='0.0.1',
     author='Sam Betts, Bradley Jones, John Davidge, Jack Peter Fletcher',
     author_email='bradleyjones92@googlemail.com',
-    packages=['hive.common', 'hive', 'hive.honeycomb', 'hive.control'],
+    packages=find_packages(),
     url='http://bradleyjones.github.io/apiary',
     license='LICENSE.txt',
     description='Middleware for the Apiery Project',
     long_description=open('README.md').read(),
     install_requires=[
-        "configobj",
-        "pika",
+        "configobj >= 4.7.2",
+        "pika >= 0.9.13",
     ],
     entry_points={
         'console_scripts':
-        ['apiary_honeycomb = hive.honeycomb.honeycomb:main',
-         'apiary_control = hive.control.control:main',
-         'apiary_intelligence = hive.control.intelligence:main',
+        ['apiary-honeycomb = hive.honeycomb.honeycomb:main',
+         'apiary-control = hive.control.control:main',
+         'apiary-intelligence = hive.control.intelligence:main',
          ]
     },
 )
