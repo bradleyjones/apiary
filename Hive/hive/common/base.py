1513564aa7639267da2d344ed7d413d5934f7ead
Sam Betts <sam@code-smash.net>
2013-10-16 16:12:35 +0100

    Generified Services to have parent Base class

---
+++ Hive/hive/common/base.py
@@ -0,0 +1,65 @@
+import logging
+from configobj import ConfigObj
+from rpcserver import RPCServer
+
+class Base(object):
+
+    def __init__(self, name):
+        self.config = self.loadConfig(name + "_config.ini")
+        self.configureLogger(
+            self.config['Logging']['location'],
+            self.config['Logging']['level'])
+
+        self.router = None
+        self.controller = None
+
+
+    def startServer(self, r, c):
+        cont = c.Controller()
+        router = r.Routes(cont)
+        logging.info("Setting Up Server on %s" % self.config['Base']['rabbit_ip'])
+        try:
+            server = RPCServer(self.config['Base']['queue_name'], self.config['Base']['rabbit_ip'], router.route)
+        except KeyboardInterrupt:
+            logging.info("Honeycomb is exiting...")
+
+    def loadConfig(self, filepath):
+        return ConfigObj(filepath)
+    
+    def configureLogger(self, filelocation, level):
+        if level == "debug":
+            logging.basicConfig(filename=filelocation,
+                                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                level=logging.DEBUG)
+        elif level == "info":
+            logging.basicConfig(filename=filelocation,
+                                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                level=logging.INFO)
+        elif level == "warning":
+            logging.basicConfig(filename=filelocation,
+                                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                level=logging.WARNING)
+        elif level == "error":
+            logging.basicConfig(filename=filelocation,
+                                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                level=logging.ERROR)
+        elif level == "critical":
+            logging.basicConfig(filename=filelocation,
+                                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                                level=logging.CRITICAL)
+        else:
+            raise Exception("Invalid Debug Level " + level)
+
+        # create console handler and set level to debug
+        logger = logging.getLogger()
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+
+        # create formatter
+        # add formatter to ch
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+
+        logger.addHandler(ch)
+        logging.info('Logging Beginning')
--- Hive/hive/honeycomb/honeycomb.py
+++ Hive/hive/honeycomb/honeycomb.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python
-from ..common.rpcserver import RPCServer
-from configobj import ConfigObj
+from ..common.base import Base
 import controller
 import routes
-from ..common import logger
-import logging
 
-
-def load_config():
-    logger.configureLogger('honeycomb.log')
-    return ConfigObj('honeycomb_config.ini')
-
+class Honeycomb(Base):
+    
+    def __init__(self):
+        Base.__init__(self, "honeycomb")
 
 def main():
-    cont = controller.Controller()
-    router = routes.Routes(cont)
-    config = load_config()
-    print "Setting Up Server on %s" % config['rabbit_ip']
-    try:
-        server = RPCServer("honeycomb", config['rabbit_ip'], router.route)
-    except KeyboardInterrupt:
-        logging.info("Honeycomb is exiting...")
+    honeycomb = Honeycomb()
+    honeycomb.startServer(routes, controller)
