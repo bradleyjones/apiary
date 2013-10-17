c726488f6e6c3baf98ea686ea286d23c6192881a
Sam Betts <sam@code-smash.net>
2013-10-17 17:12:00 +0100

    Added more error catching to rpc server

--- Hive/hive/control/routes.py
+++ Hive/hive/control/routes.py
@@ -4,5 +4,6 @@ from ..common.routes import Routes as Parent
 class Routes(Parent):
 
     def setupRoutes(self):
-        self.action("insert", self.controller.default)
+        self.action("HANDSHAKE", self.controller.handshake)
+
         self.action("wobble", self.controller.default)
