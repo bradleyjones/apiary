9895703284f141f2568e1314e00de9f2d5cbcecb
Sam Betts <sam@code-smash.net>
2013-10-22 14:19:59 +0100

    Made it so that the events exchange can be used by multiple services

--- Hive/hive/common/pubsubserver.py
+++ Hive/hive/common/pubsubserver.py
@@ -18,7 +18,11 @@ class PubSubServer(object):
         self.logger.info("Identifier: %s", self.identifier)
         self.logger.info("Publish Subscribe Server Started!")
 
-    def publish_msg(self, msg, routing_key):
+    def publish_msg(self, msg, routing_key=None):
+        if routing_key == None: 
+            routing_key = self.routing_key
+        else:
+            routing_key = self.routing_key + "." + routing_key
         self.channel.basic_publish(exchange=self.exchange,
                            routing_key=routing_key,
                            body=msg)
