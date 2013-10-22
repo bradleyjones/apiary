6b688253e8f8ec05f975da119fe4501f3017c6b0
Bradley Jones <jones.bradley@me.com>
2013-10-22 06:39:56 +0100

    Basic Publish Subsribe server running, publishing on agents topic whenever a new agent is added

--- Hive/hive/control/controller.py
+++ Hive/hive/control/controller.py
@@ -1,18 +1,32 @@
 import uuid
 import random
 import xml.etree.cElementTree as ET
-
+from ..common.pubsubserver import PubSubServer
 
 class Controller(object):
 
     def __init__(self):
         self.agents = {}
+        self.pubsub = PubSubServer('events','127.0.0.1','agents')
+
+    #Build XML for single agent
+    def xml_agent(self, data):
+        agent = ET.Element('agent')
+
+        if data in self.agents:
+          uid = ET.SubElement(agent, 'id')
+          uid.text = data
+          machid = ET.SubElement(agent, 'machineid')
+          machid.text = self.agents[data]
+
+        return ET.tostring(agent, encoding='utf8', method='xml')
 
     def handshake(self, data, resp):
         # Make sure uuid always starts with a letter because not valid
         # to have xml tag start with a number
         id = chr(random.randrange(97, 97 + 26 + 1)) + str(uuid.uuid4())
         self.agents[id] = data
+        self.pubsub.publish_msg(self.xml_agent(id), 'agents')
         resp.respond(id)
 
     def goodbye(self, data, resp):
@@ -30,15 +44,9 @@ class Controller(object):
         resp.respond(ET.tostring(response, encoding='utf8', method='xml'))
 
     def get_single_agent(self, data, resp):
-        agent = ET.Element('agent')
-
-        if data in self.agents:
-          uid = ET.SubElement(agent, 'id')
-          uid.text = data
-          machid = ET.SubElement(agent, 'machineid')
-          machid.text = self.agents[data]
-
-        resp.respond(ET.tostring(agent, encoding='utf8', method='xml'))
+        resp.respond(self.xml_agent(data))
 
     def default(self, data, resp):
         resp.respond("THIS IS THE DEFAULT ACTION")
+
+
