14e394cd11354b93bdd9bb8b9a24014ae12fdee5
Sam Betts <sam@code-smash.net>
2013-10-24 20:36:36 +0100

    Added proper agent model using sqlite to store the data

--- Hive/hive/control/agent.py
+++ Hive/hive/control/agent.py
@@ -1,9 +1,51 @@
+import sqlite3
+
 class Agent(object):
 
-  def __init__(self, id, last_heard): 
-    self.id = id
-    self.dead = False
-    self.last_heard = last_heard
-  
-  def set_dead(self): 
-    self.dead = True
+    def __init__(self, id, heartbeat, dead=False):
+        self.id = id
+        self.dead = dead
+        self.heartbeat = heartbeat
+
+class AgentModel(object):
+
+    def __init__(self, config):
+        self.conn = sqlite3.connect(config['Database']['filepath'])
+        self.c = self.conn.cursor()
+        self.c.execute('''create table if not exists Agents(
+                UUID TEXT PRIMARY KEY NOT NULL, 
+                DEAD INT NOT NULL, 
+                HEARTBEAT REAL NOT NULL)''')
+
+    def new(self, id, heartbeat):
+        return Agent(id, heartbeat)
+
+    def save(self, agent):
+        self.c.execute("SELECT * FROM Agents WHERE UUID=?", (agent.id, ))
+        one = self.c.fetchone()
+        if one == None:
+            self.c.execute("INSERT INTO Agents VALUES (?, ?, ?)", (agent.id, agent.dead, agent.heartbeat))
+        else: 
+            self.c.execute("UPDATE Agents SET DEAD=?, HEARTBEAT=?  WHERE UUID=?", (agent.dead, agent.heartbeat, agent.id))
+        self.conn.commit()
+
+    def findAll(self):
+        agents = {}
+        for row in self.c.execute("SELECT * FROM Agents"):
+            agents[row[0]] = Agent(row[0], row[2], row[1])
+        print agents
+        return agents
+
+    def delete(self, agent):
+        t = (agent.id, )
+        self.c.execute("DELETE FROM Agents WHERE UUID=?", t)
+        self.conn.commit()
+
+    def find(self, id):
+        t = (id, )
+        self.c.execute("SELECT * FROM Agents WHERE UUID=?", t)
+        one = self.c.fetchone()
+        if one == None:
+            return None
+        else:
+            return Agent(one[0], one[2], one[1]) 
--- Hive/hive/control/controller.py
+++ Hive/hive/control/controller.py
@@ -2,52 +2,53 @@ import uuid
 import random
 import json
 from ..common.controller import Controller as Parent
-from agent import Agent
+from agent import Agent, AgentModel
 import time 
 from threading import Timer
 
 class Controller(Parent):
 
     def extra_data(self):
-        self.agents = {}
+        self.agents = AgentModel(self.config)
 
     def remove_dead_agents(self):
-        for id, agent in self.agents.iteritems():
-            if agent.last_heard + 900 >= time.time():
-                agent.set_dead()
-                self.send_agent_event(agent)
+        pass
 
     def send_agent_event(self, agent):
-        event = {}
-        event[agent.id] = {}
-        event[agent.id]['dead'] = agent.dead
-        event[agent.id]['lastheard'] = agent.last_heard
+        event = self.make_agent_message(agent, {})
         self.event(json.dumps(event), 'agents')
+
+    def make_agent_message(self, agent, response):
+        response[agent.id] = {}
+        response[agent.id]['dead'] = bool(agent.dead)
+        response[agent.id]['heartbeat'] = agent.heartbeat
+        return response
 
     def handshake(self, data, resp):
         # Make sure uuid always starts with a letter because not valid
         # to have xml tag start with a number
         id = str(uuid.uuid4())
         agent = Agent(id, time.time())
-        self.agents[id] = agent
+        self.agents.save(agent)
         self.send_agent_event(agent)
         resp.respond(id)
 
     def goodbye(self, data, resp):
-        del self.agents[data]
+        agent = self.agents.find(data)
+        self.agents.delete(agent)
         resp.respond("GOODBYE!")
 
     def get_agents(self, data, resp):
+        agents = self.agents.findAll()
         response = {}
-        response['agents'] = {}
-        for agent in self.agents:
-            response['agents'][agent.id] = {}
-            response['agents'][agent.id]['dead'] = agent.dead
-            response['agents'][agent.id]['lastheard'] = agent.last_heard
+        for key, agent in agents.iteritems():
+            response = self.make_agent_message(agent, response)
         resp.respond(json.dumps(response))
 
     def get_single_agent(self, data, resp):
-        resp.respond(json.dumps(self.agent[data]))
+        agent = self.agents.find(data)
+        response = self.make_agent_message(agent, {})
+        resp.respond(json.dumps(response))
 
     def default(self, data, resp):
         resp.respond("THIS IS THE DEFAULT ACTION")
