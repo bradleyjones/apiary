d6a0add8a334821b4cd4949436665069b161a4a6
Bradley Jones <jones.bradley@me.com>
2013-11-18 23:57:56 +0000

    Added sessions support, stored in mongodb

--- Queen/server.js
+++ Queen/server.js
@@ -1,28 +1,44 @@
 "use strict";
 
 // Setup all the required libraries
-var http = require('http');
-var express = require('express');
-var app = express();
-var server = http.createServer(app).listen(3000);
-var jade = require('jade');
-var io = require('socket.io').listen(server);
+var http = require('http')
+  , express = require('express')
+  , app = express()
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server)
+  , MongoStore = require('connect-mongo')(express)
+  , mongoose = require('mongoose')
+  , User = require('./models/user');
 
 exports.io = io;
 
 // Setup express to use views and public folder
 // Use Jade as the template engine for views
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.set("view options", { layout: false });
 app.configure(function () {
-    app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set("view options", { layout: false });
+  app.use(express.methodOverride());
+  // Use mongodb to store sessions
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: 'THISISASECRETSSHHH',
+    store: new MongoStore({
+      db: 'queensessions'
+    })
+  }));
+  app.use(express.static(__dirname + '/public'));
 });
 
 // Render the home page
 app.get('/', function (req, res) {
     res.render('home.jade');
 });
+
+// Start listening
+server.listen(3000, function(){
+  console.log("Queen is listening on port %d", server.address().port)
+})
 
 /*
   Routes
