f9712f3dce53102b3f219c1a802e1b9c09a190d9
Bradley Jones <jones.bradley@me.com>
2013-11-19 02:01:57 +0000

    Fix json error in user schema

--- Queen/models/user.js
+++ Queen/models/user.js
@@ -5,7 +5,7 @@ var mongoose = require("mongoose")
 
 var UserSchema = new Schema({
   username: { type: String, required: true, index: {unique: true}},
-  password: { type: String, required: true},
+  password: { type: String, required: true}
 });
 
 UserSchema.pre('save', function(next) {
