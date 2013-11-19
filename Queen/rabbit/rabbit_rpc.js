f8272f965eb481ba29ce942f43ed2f6e86f6e4dc
Bradley Jones <jones.bradley@me.com>
2013-11-19 21:54:30 +0000

    removed broken rpc timeout

--- Queen/rabbit/rabbit_rpc.js
+++ Queen/rabbit/rabbit_rpc.js
@@ -3,7 +3,6 @@
 var config = require('../config/config')
   , amqp = require('amqp')
   , crypto = require('crypto')
-  , TIMEOUT = 4000 //ms time to wait
   , CONTENT_TYPE = 'application/json'
   , colors = require('colors');
 
@@ -15,17 +14,12 @@ exports.RPCQuery = function (queueName, data, callback) {
     self.responseQueue = false;
     connection.on("ready", function () {
         self.correlationId = crypto.randomBytes(16).toString('hex');
-        var timeout = setTimeout(function (corr_id) {
-                console.error(new Error("timeout " + corr_id).red);
-                //connection.end();
-            }, TIMEOUT, self.correlationId);
 
         connection.queue('', {exclusive: true}, function (queue) {
             self.responseQueue = queue.name;
             queue.subscribe(function (message, headers, deliveryInfo, m) {
                 try {
                     if (self.correlationId === m.correlationId) {
-                        clearTimeout(timeout);
                         connection.end();
                         var msg = message['data'].toString('utf-8');
                         callback(JSON.parse(msg));
