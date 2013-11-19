5e732f48ac02ead825b0cbe98863e9b24e8a5305
Bradley Jones <jones.bradley@me.com>
2013-11-19 00:37:44 +0000

    plaintext authentication

--- Queen/server.js
+++ Queen/server.js
@@ -18,6 +18,8 @@ app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.set("view options", { layout: false });
+  app.use(express.json());
+  app.use(express.urlencoded());
   app.use(express.methodOverride());
   // Use mongodb to store sessions
   app.use(express.cookieParser());
@@ -27,12 +29,8 @@ app.configure(function () {
       db: 'queensessions'
     })
   }));
+  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
-});
-
-// Render the home page
-app.get('/', function (req, res) {
-    res.render('home.jade');
 });
 
 // Start listening
@@ -41,8 +39,45 @@ server.listen(3000, function(){
 })
 
 /*
-  Routes
-*/
+ *  Authentication
+ */
+
+function checkAuth(req, res, next) {
+  if (!req.session.user_id) {
+    res.render('login.jade');
+  } else {
+    // Stop caching of restricted pages
+    res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next();
+  }
+}
+
+app.post('/login', function (req, res) {
+  var post = req.body;
+  console.log(post);
+  if (post.user == 'user' && post.password == 'pass') {
+    req.session.user_id = 1;
+    res.redirect('/');
+  } else {
+    res.send('Bad user/pass');
+  }
+});
+
+app.get('/logout', function (req, res) {
+  delete req.session.user_id;
+  res.redirect('/');
+});
+
+
+/*
+ *  Routes
+ */
+
+// Render the home page
+app.get('/', checkAuth, function (req, res) {
+    res.render('home.jade');
+});
+
 var agents = require('./controllers/agents');
 
 app.get('/agents', agents.list);
