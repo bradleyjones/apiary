d2e30365afcef2756df7682c6403949cc52f7263
John Davidge <jodavidg@cisco.com>
2013-11-21 15:15:31 +0000

    Cleaned up the Sting channel creation process.

--- Hive/hive/sting/sting.py
+++ Hive/hive/sting/sting.py
@@ -15,10 +15,10 @@ def callback(ch, method, properties, body):
     alert_text = "NONE"
     data = json.loads(body)
     for agent in data.values():
-      if agent["DEAD"]:
-        alert_text = "Agent " + agent["UUID"] + " is Dead!"
-      else:
-        alert_text = "New Agent: " + agent["UUID"]
+        if agent["DEAD"]:
+          alert_text = "Agent " + agent["UUID"] + " is Dead!"
+        else:
+          alert_text = "New Agent: " + agent["UUID"]
 
     # Send a notification
     # Need to implement database of registered users and their devices
@@ -28,7 +28,24 @@ def callback(ch, method, properties, body):
     # For each message for each user
     apns.gateway_server.send_notification(token_hex, payload)
 
-    print alert_text
+    print "Notification Sent: " + alert_text
+
+def new_channel(connection, routing_key_name):
+
+    # Initialise channel
+    channel = connection.channel()
+
+    # Configure channel
+    channel.exchange_declare(exchange='apiary', type='topic')
+
+    result = channel.queue_declare(exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange='apiary', queue=queue_name, routing_key=routing_key_name)
+
+    channel.basic_consume(callback, queue=queue_name, no_ack=False)
+
+    return channel
 
 def main():
 
@@ -36,31 +53,9 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
 
     # Define channels to listen on
-    new_agent_channel = connection.channel()
-    dead_agent_channel = connection.channel()
-
-    # Configure new_agent_channel
-    new_agent_channel.exchange_declare(exchange='apiary', type='topic')
-
-    result = new_agent_channel.queue_declare(exclusive=True)
-    queue_name = result.method.queue
-
-    new_agent_channel.queue_bind(exchange='apiary', queue=queue_name, routing_key='events.agentmanager.agent.new')
-
-    new_agent_channel.basic_consume(callback, queue=queue_name, no_ack=False)
-
-    # Configure dead_agent_channel
-    dead_agent_channel.exchange_declare(exchange='apiary', type='topic')
-
-    result = dead_agent_channel.queue_declare(exclusive=True)
-    queue_name = result.method.queue
-
-    dead_agent_channel.queue_bind(exchange='apiary', queue=queue_name, routing_key='events.agentmanager.agent.dead')
-
-    dead_agent_channel.basic_consume(callback, queue=queue_name, no_ack=False)
+    new_agent_channel = new_channel(connection, 'events.agentmanager.agent.new')
+    dead_agent_channel = new_channel(connection, 'events.agentmanger.agent.dead')
 
     # Start channel consumers
     new_agent_channel.start_consuming()
-    print "Listenting for new agents on events.agentmanager.agent.new"
     dead_agent_channel.start_consuming()
-    print "Listenting for dead agents on events.agentmanager.agent.dead"
