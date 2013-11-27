f313073f94159a09fcc3e9324da7959b4fff1a40
Sam Betts <sam@code-smash.net>
2013-11-27 12:25:09 +0000

    Updated start script to use localrc,

--- Hive/start.sh
+++ Hive/start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if [ -z "$MULTIPLEXER" ]; then
-  MULTIPLEXER="SCREEN" #SCREEN   Or  TMUX
-fi
+source ./localrc
+
+function add_to_screen {
+  NL=`echo -ne '\015'`
+  echo "Starting ... $1"
+  screen -S $SCREEN_NAME -X screen -t $1
+  sleep 1.5
+  screen -S $SCREEN_NAME -p $1 -X stuff "$1$NL"
+}
 
 if [ $MULTIPLEXER == SCREEN ]; then
-  echo "$MULTIPLEXER"
   SCREEN_NAME=${SCREEN_NAME:-hive}
 
   if type -p screen >/dev/null && screen -ls | egrep -q "[0-9].$SCREEN_NAME"; then
@@ -23,16 +28,12 @@ if [ $MULTIPLEXER == SCREEN ]; then
   fi
 
   screen -r $SCREEN_NAME -X hardstatus alwayslastline "$SCREEN_HARDSTATUS"
-  #screen -r $SCREEN_NAME -X setenv PROMPT_COMMAND "HIVEMODE"
 
-  NL=`echo -ne '\015'`
-
-  function add_to_screen {
-    echo "Starting ... $1"
-    screen -S $SCREEN_NAME -X screen -t $1
-    sleep 1.5
-    screen -S $SCREEN_NAME -p $1 -X stuff "$1$NL"
-  }
+  if $DEAMONS; then 
+    add_to_screen "mongod"
+    add_to_screen "rabbitmq-server"
+    sleep 1
+  fi
 
   add_to_screen "apiary-honeycomb"
   add_to_screen "apiary-agentmanager"
@@ -40,25 +41,34 @@ if [ $MULTIPLEXER == SCREEN ]; then
   add_to_screen "apiary-sting"
 
 elif [ $MULTIPLEXER == TMUX ]; then
-  echo "$MULTIPLEXER is better :D"
   NAME=Hive
 
   tmux has-session -t $NAME
   if [ $? != 0 ]
   then
     tmux new-session -s $NAME -d
+    
+    if $DEAMONS; then 
+      tmux new-window -n mongo -t $NAME:1
+      tmux send-keys -t $NAME:1 "mongod" C-m
 
-    tmux new-window -n control -t $NAME:2
-    tmux send-keys -t $NAME:2 "apiary-agentmanager" C-m
+      tmux new-window -n rabbit -t $NAME:2
+      tmux send-keys -t $NAME:2 "rabbitmq-server" C-m
 
-    tmux new-window -n control-statemachine -t $NAME:3
-    tmux send-keys -t $NAME:3 "apiary-agentmonitor" C-m
+      sleep 1
+    fi
 
-    tmux new-window -n honeycomb -t $NAME:4
-    tmux send-keys -t $NAME:4 "apiary-honeycomb" C-m
+    tmux new-window -n control -t $NAME:3
+    tmux send-keys -t $NAME:3 "apiary-agentmanager" C-m
 
-    tmux new-window -n sting -t $NAME:5
-    tmux send-keys -t $NAME:5 "apiary-sting" C-m
+    tmux new-window -n control-statemachine -t $NAME:4
+    tmux send-keys -t $NAME:4 "apiary-agentmonitor" C-m
+
+    tmux new-window -n honeycomb -t $NAME:5
+    tmux send-keys -t $NAME:5 "apiary-honeycomb" C-m
+
+    tmux new-window -n sting -t $NAME:6
+    tmux send-keys -t $NAME:6 "apiary-sting" C-m
 
     tmux select-window -t $NAME:1
   fi
