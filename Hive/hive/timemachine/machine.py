b40cb437e5726a5db339796f815e8164a1657cc5
Sam Betts <sam@code-smash.net>
2013-12-03 13:46:47 +0000

    Comments add to timemachine machine writer clas

--- Hive/hive/timemachine/machine.py
+++ Hive/hive/timemachine/machine.py
@@ -46,24 +46,26 @@ class Writer(threading.Thread):
         self.queue = None
 
     def run(self):
+        # Connect to rabbit
         credentials = pika.PlainCredentials(self.config['Rabbit']['username'], self.config['Rabbit']['password'])
         self.connection = pika.BlockingConnection(
            pika.ConnectionParameters(host=self.config['Rabbit']['host'], credentials=credentials))
         self.channel = self.connection.channel() 
 
+        # Setup queue for outputing data
         result = channel.queue_declare(exclusive=true)
         self.queue = result.method.queue
-
-        result = channel.queue_declare(exclusive=true)
-        self.buff = result.method.queue
-        channel.queue_bind(exchange='apiary', queue=self.buff, routing_key='agent.*.data')
-
+        
+        # Push all historic data from honeycomb
         rpcSender = RPCSender(self.config)
-  
         resp = rpcSender.send_request('THINGS', 'honeycomb', '{}', 'SOMETHING', 'timemachineworker', key='honeycomb')
         for log in resp.data:
           self.channel.basic_publish(exchange='', routing_key=self.queue, body=log)
-      
+     
+        # Start listening for live data to forward
+        result = channel.queue_declare(exclusive=true)
+        self.buff = result.method.queue
+        channel.queue_bind(exchange='apiary', queue=self.buff, routing_key='agent.*.data')
         channel.basic_consume(on_message, queue=self.buff, no_ack=True)
         channel.start_consuming()
 
