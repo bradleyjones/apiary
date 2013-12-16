f672b763390822f555c926b31a72e5b594819f73
Sam Betts <sam@code-smash.net>
2013-12-16 14:25:08 +0000

    Added .orig files to git ignore, Added my attempt to script pylucene install

---
+++ Hive/install_pylucene.py
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export JCC_JDK=""
+
+wget "http://mirror.ox.ac.uk/sites/rsync.apache.org/lucene/pylucene/pylucene-4.5.1-1-src.tar.gz"
+tar xzf "pylucene-4.5.1-1-src.tar.gz"
+cd "pylucene-4.5.1-1-src.tar.gz"
+
+pushd jcc
+
+sudo python setup.py build
+sudo python setup.py install
+
+popd 
+
+vim Makefile
+
+make
+
+sudo make test 
+
+sudo make install 
