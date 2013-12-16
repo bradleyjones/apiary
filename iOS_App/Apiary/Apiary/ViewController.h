a2d0cdd885c28bf65a80958ad0632e37704ef280
John <jodavidg@cisco.com>
2013-12-16 14:29:11 +0000

    Began implementation of native in-app login.

--- iOS_App/Apiary/Apiary/ViewController.h
+++ iOS_App/Apiary/Apiary/ViewController.h
@@ -8,6 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+
+    IBOutlet UITextField *usernameField;
+    IBOutlet UITextField *passwordField;
+}
+- (IBAction)usernameFieldDismiss:(id)sender;
+- (IBAction)passwordFieldDismiss:(id)sender;
 
 @end
