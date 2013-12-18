b861f57b7540b35b034c9a330bb0c109c144f920
John <jodavidg@cisco.com>
2013-12-18 12:05:18 +0000

    Began implementing data persistence between app shutdown/load.

--- iOS_App/Apiary/Apiary/DataClass.m
+++ iOS_App/Apiary/Apiary/DataClass.m
@@ -12,6 +12,8 @@
 @synthesize user;
 @synthesize password;
 @synthesize url;
+@synthesize filePath;
+@synthesize data_storage;
 static DataClass *instance =nil;
 +(DataClass *)getInstance
 {
@@ -20,6 +22,12 @@ static DataClass *instance =nil;
         if(instance==nil)
         {
             instance= [DataClass new];
+            
+            NSArray *directories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+            NSString *documents = [directories lastObject];
+            instance.filePath = [documents stringByAppendingPathComponent:@"data_storage.plist"];
+            instance.data_storage = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"URL", @"", @"username", @"", @"password", @"", nil];
+            [instance.data_storage writeToFile:instance.filePath atomically:YES];
         }
     }
     return instance;
