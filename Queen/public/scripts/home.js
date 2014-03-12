c6d153554c58eef6c305bfa6c93451e1f398e9c0
Jack Peter Fletcher <jackfletcher2010@googlemail.com>
2014-03-12 14:05:00 +0000

    Added button for search on home screen

--- Queen/public/scripts/home.js
+++ Queen/public/scripts/home.js
@@ -3,3 +3,9 @@ var socket = io.connect('http://localhost/home');
 socket.on('agentcount', function(data) {
   $("#agentcount").html("<h3>Number of Agents : <b>" + data + "</b></h3>")
 });
+
+$(function() {
+   $("#searchButton").click(function(){
+      console.log("hey");
+   });
+});
