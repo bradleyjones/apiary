55af6636215057c8f29983e0d6333e7f0e7a76a6
John <jodavidg@cisco.com>
2014-03-15 15:56:23 +0000

    Added DataClass for storing user input.

---
+++ iOS_App/Apiary/Apiary/DataClass.h
@@ -0,0 +1,13 @@
+//
+//  DataClass.h
+//  Apiary
+//
+//  Created by John Davidge on 15/03/2014.
+//  Copyright (c) 2014 John Davidge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataClass : NSData
+
+@end
---
+++ iOS_App/Apiary/Apiary/DataClass.m
@@ -0,0 +1,13 @@
+//
+//  DataClass.m
+//  Apiary
+//
+//  Created by John Davidge on 15/03/2014.
+//  Copyright (c) 2014 John Davidge. All rights reserved.
+//
+
+#import "DataClass.h"
+
+@implementation DataClass
+
+@end
