c94337976bf23fa8f976ffa42b6b5ebe8f8a6dfb
John <jodavidg@cisco.com>
2014-03-15 16:14:48 +0000

    Implemented storage and retrieval of usernames/passwords.

--- iOS_App/Apiary/Apiary/ViewController.m
+++ iOS_App/Apiary/Apiary/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "DataClass.h"
 
 @interface ViewController ()
 
@@ -14,10 +15,41 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    // Get an instance of DataClass
+    DataClass *obj=[DataClass getInstance];
+    // Get username and password from storage
+    obj.user = [obj.data_storage objectForKey:@"username"];
+    obj.password = [obj.data_storage objectForKey:@"password"];
+    // Set textField properties
+    usernameField.text = obj.user;
+    passwordField.text = obj.password;
+}
+
+- (IBAction)usernameFieldDismiss:(id)sender {
+    // Get an instance of DataClass
+    DataClass *obj=[DataClass getInstance];
+    // Set the username and save it
+    obj.user = usernameField.text;
+    [obj.data_storage setValue:obj.user forKey:@"username"];
+    [obj.data_storage writeToFile:obj.filePath atomically:YES];
+    // Exit the textField
+    [usernameField resignFirstResponder];
+}
+
+- (IBAction)passwordFieldDismiss:(id)sender {
+    // Get and instance of DataClass
+    DataClass *obj=[DataClass getInstance];
+    // Set the password and save it
+    obj.password = passwordField.text;
+    [obj.data_storage setValue:obj.password forKey:@"password"];
+    [obj.data_storage writeToFile:obj.filePath atomically:YES];
+    // Exit the textField
+    [passwordField resignFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
