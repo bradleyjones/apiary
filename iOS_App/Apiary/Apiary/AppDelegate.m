b895e2c1ff644dfc02f9f70a7ea14972faf2b5c6
John <jodavidg@cisco.com>
2014-03-15 16:02:42 +0000

    Implemented retrieval and storage of Device ID.

--- iOS_App/Apiary/Apiary/AppDelegate.m
+++ iOS_App/Apiary/Apiary/AppDelegate.m
@@ -7,12 +7,15 @@
 //
 
 #import "AppDelegate.h"
+#import "DataClass.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    // Request push notifications
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+     (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
     return YES;
 }
 							
@@ -41,6 +44,30 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+{
+    // Convert NSData object into correctly formatted NSString
+    const unsigned *tokenBytes = [deviceToken bytes];
+    NSString *hexToken = [NSString stringWithFormat:@"%08x%08x%08x%08x%08x%08x%08x%08x",
+                          ntohl(tokenBytes[0]), ntohl(tokenBytes[1]), ntohl(tokenBytes[2]),
+                          ntohl(tokenBytes[3]), ntohl(tokenBytes[4]), ntohl(tokenBytes[5]),
+                          ntohl(tokenBytes[6]), ntohl(tokenBytes[7])];
+    
+    // Debug output
+	NSLog(@"My token is: %@", hexToken);
+    
+    // Save device_id to disk
+    DataClass *obj=[DataClass getInstance];
+    obj.device_id = hexToken;
+    [obj.data_storage setValue:obj.device_id forKey:@"device_id"];
+    [obj.data_storage writeToFile:obj.filePath atomically:YES];
+}
+
+- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
+{
+	NSLog(@"Failed to get token, error: %@", error);
 }
 
 @end
