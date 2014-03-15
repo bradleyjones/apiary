e11c341b7933859af1ccc8c94c10a98c0de0c170
John <jodavidg@cisco.com>
2014-03-15 15:53:15 +0000

    Added custom view controllers for settings and web screens.

---
+++ iOS_App/Apiary/Apiary/ApiaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  ApiaryViewController.h
+//  Apiary
+//
+//  Created by John Davidge on 15/03/2014.
+//  Copyright (c) 2014 John Davidge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApiaryViewController : UIViewController
+
+@end
