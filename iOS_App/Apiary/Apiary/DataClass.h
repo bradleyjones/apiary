6a78c5e319db92d94dbdf463077f3795e91b30e5
John <jodavidg@cisco.com>
2014-03-15 15:58:11 +0000

    Implemented DataClass functions.

--- iOS_App/Apiary/Apiary/DataClass.h
+++ iOS_App/Apiary/Apiary/DataClass.h
@@ -8,6 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-@interface DataClass : NSData
-
+@interface DataClass : NSObject {
+    NSString *user;
+    NSString *password;
+    NSString *url;
+    NSString *device_id;
+    NSString *filePath;
+    NSMutableDictionary *data_storage;
+}
+@property(nonatomic,retain)NSString *user;
+@property(nonatomic,retain)NSString *password;
+@property(nonatomic,retain)NSString *url;
+@property(nonatomic,retain)NSString *device_id;
+@property(nonatomic,retain)NSString *filePath;
+@property(nonatomic,retain)NSMutableDictionary *data_storage;
++(DataClass*)getInstance;
 @end
