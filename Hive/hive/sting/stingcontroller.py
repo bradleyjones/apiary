b44c8ad2f9bd52d0baa4b446b5226886a6089e53
johndavidge <jodavidg@cisco.com>
2014-03-18 17:26:55 +0000

    APNS notifications now working with new user system.

--- Hive/hive/sting/stingcontroller.py
+++ Hive/hive/sting/stingcontroller.py
@@ -12,79 +12,22 @@ class Controller(Parent):
 
     def models(self):
         self.users = user(self.config)
+        self.devices = device(self.config)
 
     # BELOW THIS LINE ARE ALL CONTROLLER ACTIONS
 
-    def callback(ch, method, properties, body):
-        # Initialise Apple Push Notification System object
-        apns = APNS(use_sandbox=True, cert_file=(
-        resource_filename(__name__, 'apns/certs/ApiaryCert.pem')),
-        key_file=(resource_filename(__name__, 'apns/certs/ApiaryKey.pem')))
-
-        # Initialise String for message to be sent in alert
-        alert_text = "None"
-
-        # Load data from json recieved on message queue
-        data = json.loads(body)
-
-        # This implementation only looks at new/dead agents
-        # Once user-definable alerts are implemented this will be extended
-        for agent in data.values():
-            uuid = agent["UUID"]
-            if agent["DEAD"]:
-                alert_text = "Agent " + uuid + " is Dead!"
-            else:
-                alert_text = "New Agent: " + uuid
-
-        # Send the notification
-        # Once userdefinable alerts are implemented the user's device will
-        # be identified based on the user associated with the alert
-        # For now we're using a single device
-        token_hex = 'c337dce1b94e8908e3b9768516fed42c90b1dff0ad01dfe7334970227da0a229'
-        payload = Payload(alert=alert_text, sound="default", badge=1)
-        apns.gateway_server.send_notification(token_hex, payload)
-
-        print "Notification Sent: " + alert_text
-
-#def new_channel(connection, routing_key_name):
-#
-## Initialise channel
-#channel = connection.channel()
-#
-## Configure channel
-#channel.exchange_declare(exchange='apiary', type='topic')
-#
-#result = channel.queue_declare(exclusive=True)
-#queue_name = result.method.queue
-#
-#channel.queue_bind(
-#exchange='apiary',
-#queue=queue_name,
-#routing_key=routing_key_name)
-#
-#channel.basic_consume(callback, queue=queue_name, no_ack=False)
-#
-#return channel
-
-    def event():
+    def event(self, message, response):
 
         print "HELLOOOOOOO"
 
         for user in self.users.findAll():
             for device in user.devices:
-                apns = APNS(use_sandbox=True, cert_file=(
+                device = self.devices.find(device)
+                apns = APNs(use_sandbox=True, cert_file=(
                 resource_filename(__name__,'apns/certs/ApiaryCert.pem')),
                 key_file=(resource_filename(__name__,'apns/certs/ApiaryKey.pem')))
                 token_hex = device.device_id
-                payload = Payload(alert="Sting Running", sound="default", badge=0)
+                payload = Payload(alert="Sting Running", sound="default", badge=1)
                 apns.gateway_server.send_notification(token_hex, payload)
 
-# Debug APNS Notification
-# apns = APNS(use_sandbox=True, cert_file=(
-#    resource_filename(__name__,'apns/certs/ApiaryCert.pem')),
-#    key_file=(resource_filename(__name__,'apns/certs/ApiaryKey.pem')))
-# token_hex = 'c337dce1b94e8908e3b9768516fed42c90b1dff0ad01dfe7334970227da0a229'
-# payload = Payload(alert="Sting Running", sound="default", badge=0)
-# apns.gateway_server.send_notification(token_hex, payload)
-#########################
 
