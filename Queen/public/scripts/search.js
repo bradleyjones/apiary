ad13e79b56fd9eb3cad2236489c3de5caa5e1169
Jack Peter Fletcher <jackfletcher2010@googlemail.com>
2014-03-20 16:44:11 +0000

    Added Lucene query validation

--- Queen/public/scripts/search.js
+++ Queen/public/scripts/search.js
@@ -4,9 +4,10 @@ $(function() {
    $("#searchButton").click(function(){
       searchTerm = $("#searchTerm")[0].value;
       console.log(searchTerm);
-
-      //Edit so data sources and other stuff is also send down with data, Bro
-      socket.emit('querySubmit', searchTerm);
+      if(doCheckLuceneQueryValue(searchTerm)){
+        //Edit so data sources and other stuff is also send down with data, Bro
+        socket.emit('querySubmit', searchTerm);
+      }
    });
 });
 
