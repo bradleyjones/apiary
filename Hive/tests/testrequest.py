38a8b3f886da72849c601e0d5a6d6e3c38ee6c87
johndavidge <jodavidg@cisco.com>
2014-03-20 22:48:18 +0000

    STING AND PHEROMONE ARE WORKING IN HARMONY ARRRRGHHHHHHHH

--- Hive/tests/testrequest.py
+++ Hive/tests/testrequest.py
@@ -13,11 +13,11 @@ data['TIMESCALE'] = "86400"
 data['files'] = {'PATH': "/etc/path", 'TAGS': "firewall"}
 data['agents'] = ['17af7719-f759-44a5-95a6-f66d31c53f86']
 
-data['query'] = "CONTENT:\"ERROR *.*.*.*\""
+data['query'] = "CONTENT:apple"
 data['time'] = 600
-data['quantity'] = 10
+data['quantity'] = 5
 data['message'] = "HOLY MOTHER OF JEBUS"
-data['user'] = "insertauserid"
+data['user'] = "532b69a3dada5ac85667ba66"
 
 sdata = data
  
@@ -25,11 +25,11 @@ config = {}
 config['Rabbit'] = {}
 config['Rabbit']['username'] = 'guest'
 config['Rabbit']['password'] = 'guest'
-config['Rabbit']['host'] = '192.168.1.106'
+config['Rabbit']['host'] = '127.0.0.1'
 
 sender = RPCSender(config)
 
-resp = sender.send_request('DATA', 'hive', sdata, 'LKJABSDHBAS', 'Test Script', exchange="", key="honeycomb")
-#resp = sender.send_request('NEW', 'hive', sdata, 'LKJABSDHBAS', 'Test Script', exchange="", key="pheromone")
+#resp = sender.send_request('DATA', 'hive', sdata, 'LKJABSDHBAS', 'Test Script', exchange="", key="honeycomb")
+resp = sender.send_request('NEW', 'hive', sdata, 'LKJABSDHBAS', 'Test Script', exchange="", key="pheromone")
 
 print resp
