a824ce7c4996bed071849f7081de82daf3411661
Jack Peter Fletcher <jackfletcher2010@googlemail.com>
2014-03-20 22:58:25 +0000

    Pie chart stuff so close

--- Queen/controllers/search.js
+++ Queen/controllers/search.js
@@ -17,11 +17,44 @@ io.of('/search').on('connection', function (socket) {
 
     var msg = rabbit.constructQuery('QUERY', 'honeycomb', search)
     new rabbit.rpc('honeycomb', msg, function (data) {
-      console.log("TOTAL HITS RETURNED IS: " + data.data.totalhits);
+      console.log("TOTAL HITS RETURNED IS: " + data.data.totalHits);
       console.log("RETURNED DATA FROM QUERY IS: " + data.data.hits);
 
-      socket.emit('results', data.data.hits)
+      socket.emit('results', data.data.hits);
     });
+  });
+
+  //HACK CODE, PLZ REMOVE
+  socket.on("pieQuerySubmit", function(searches){
+    console.log("Query for Pie Chart");
+   
+    runningResults = [];
+
+    function recursiveQuery(searches, runningResults){
+      console.log("Subseach Number" + searches.main.subsearch.length);
+      if(searches.main.subsearch.length == 0){
+        socket.emit('pieHackResults', runningResults);
+      } else {
+        console.log("Performing Search Iteration");
+        //popSubSearch
+        subsearch = searches.main.subsearch.pop();
+        search = searches.main.search;
+        //Append to searchi
+        subsearchLabel = subsearch.label;
+        subsearch = "("+search+") AND ("+subsearch.subQuery+")";
+
+        //send to honeycomb
+        var msg = rabbit.constructQuery('QUERY', 'honeycomb', subsearch)
+        new rabbit.rpc('honeycomb', msg, function(data){
+          console.log("TOTAL HITS RETURNED IS: " + data.data.totalHits);
+          console.log("RETURNED DATA FROM QUERY IS: " + data.data.hits);
+
+          runningResults.push({label: subsearchLabel, value: data.data.totalHits});
+          recursiveQuery(searches, runningResults);
+        });
+      }
+    }
+    recursiveQuery(searches, runningResults);
   });
 
   // Get the current tags
