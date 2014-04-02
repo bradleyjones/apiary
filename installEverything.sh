b3e8d8f149196820d405b022789e9b2b76c51ebe
John <jodavidg@cisco.com>
2014-04-02 23:09:21 +0100

    Added more dependencies.

--- installEverything.sh
+++ installEverything.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 echo "Installing Dependencies..."
 sudo apt-get update
-sudo apt-get install -y python-software-properties python g++ make gcc make ant openjdk-7-jdk python-dev
+sudo apt-get install -y python-software-properties python g++ make gcc make ant openjdk-7-jdk python-dev curl
 sudo curl http://npmjs.org/install.sh -L -o -| sudo sh
 
 echo "Installing Hive..."
