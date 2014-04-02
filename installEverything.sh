4206640a38eedc702cc930f94e68a3d26995924e
Sam Betts <sam@code-smash.net>
2014-04-02 20:04:29 +0100

    Install Everything Script

---
+++ installEverything.sh
@@ -0,0 +1,24 @@
+echo "Installing Dependencies..."
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make gcc make ant openjdk-7-jdk python-dev
+sudo curl http://npmjs.org/install.sh -L -o -| sudo sh
+
+echo "Installing Hive..."
+pushd Hive
+python ez_setup.py
+./install.sh
+popd 
+echo "Done..."
+
+echo "Installing Queen..."
+pushd Queen
+npm install
+popd
+echo "Done..."
+
+echo "Installing Bee..."
+pushd Agents/BEE\ Client
+npm install 
+popd
+echo "Done..."
+
